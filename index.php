838b61e5aa3b78f439432416b147d89bac517ebc
Darklaneanjana <darklaneanjana@gmail.com>
2021-07-20 21:18:24 +0530

    1

--- index.php
+++
@@ -1,3 +0,0 @@
-<h1> hee hee hee </h1>
-<img src="cat1.jpg" /> 
-this is a cat
---
+++ src/servlets/AddServlet.java
@@ -0,0 +1,61 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddServlet
+ */
+@WebServlet("/AddServlet")
+public class AddServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int no1 = Integer.parseInt(request.getParameter("no1"));
+		int no2 = Integer.parseInt(request.getParameter("no2"));
+		
+		int sum = no1 + no2;
+		
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/Results.jsp");
+		request.setAttribute("sum", sum);
+		dispatcher.forward(request, response);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
